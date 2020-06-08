c0e40676bb0bebc68e351eeb99cb2cb7b60020a5
Michael Rowland <mrowland13@gmail.com>
2020-06-08 15:39:30 -0700

    queue part complete

--- queue/queue.py
+++ queue/queue.py
@@ -1,3 +1,5 @@
+from linked_list import LinkedList
+
 """
 A queue is a data structure whose primary purpose is to store and
 return elements in First In First Out order. 
@@ -13,16 +15,45 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+# class Queue:
+#     def __init__(self, storage=[]):
+#         self.size = 0
+#         self.storage = storage
+    
+#     def __len__(self):
+#         if not self.storage:
+#             return 0
+#         return len(self.storage)
+
+#     def enqueue(self, value):
+#         return self.storage.insert(0, value)
+
+#     def dequeue(self):
+#         if not self.storage:
+#             return None
+#         return self.storage.pop()
+
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
     
     def __len__(self):
-        pass
+        if not self.storage.head:
+            return 0
+        if self.storage.head is self.storage.tail:
+            return 1
+        current = self.storage.head
+        # WE PUT THIS IN BECAUSE WE KNOW THERE IS AT LEAST 1 AND OUR WHILE LOOP
+        # LOOKS AT THE NEXT ITEM
+        self.size = 1
+        while current.get_next() is not None:
+            current = current.get_next()
+            self.size += 1
+        return self.size
 
     def enqueue(self, value):
-        pass
+        return self.storage.add_to_tail(value)
 
     def dequeue(self):
-        pass
+        return self.storage.remove_head()
